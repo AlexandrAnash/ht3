044a7a1a0a1bffc2660037527a8f999b94324d48
Alexandr Anashkin <alexanash92@gmail.com>
2016-07-22 10:02:04 +0300

    start

--- SHRI.MovieSilencer/client-src/js/app.js
+++ SHRI.MovieSilencer/client-src/js/app.js
@@ -1,86 +1,65 @@
-﻿import {SubtitlesService} from './subtitlesService';
-import {SubtitlesFactory} from './subtitlesFactory';
+﻿import {SubtitleProcessor} from './subtitleProcessor';
 import {OverlayEffect} from './overlayEffect';
 import {Player} from './player';
 import {Audio} from './audio';
 import {Subtitle} from './Subtitle';
 
 document.addEventListener('DOMContentLoaded', ContentLoaded);
+
 function ContentLoaded() {
-    const subtitlesFactory = new SubtitlesFactory();
-    return Promise.all([
-        subtitlesFactory.loadSubtitles('attachment/subs.srt'),
-    ]).then(function(promiseData) {
-        const player = new Player();
-        const audio  = new Audio();
-        const subtitles = promiseData[0];
-        const video = player.video;
-        const overlayEffect = new OverlayEffect();
-        const overlayVideo = overlayEffect.video;
+    const player = new Player();
+    const audio = new Audio();
+    const video = player.video;
+    const overlayEffect = new OverlayEffect();
+    const overlayVideo = overlayEffect.video;
 
-        var subtitlesService = new SubtitlesService(subtitles, player);
+    var subtitleProcessor = new SubtitleProcessor(player);
 
-        let elementPlay = document.getElementById('player-play');
-        let elementPause = document.getElementById('player-pause');
-        let elementRestart = document.getElementById('player-restart');
-        let rifID;
-        elementPlay.addEventListener('click', playPress.bind(this));
-        elementPause.addEventListener('click', pausePress.bind(this));
-        elementRestart.addEventListener('click', restartPress.bind(this));
+    const elementPlay = document.getElementById('player-play');
+    const elementPause = document.getElementById('player-pause');
+    const elementRestart = document.getElementById('player-restart');
+    let rifID;
+    elementPlay.addEventListener('click', playPress.bind(this));
+    elementPause.addEventListener('click', pausePress.bind(this));
+    elementRestart.addEventListener('click', restartPress.bind(this));
 
-        function playPress() {
-            elementPlay.classList.add('btn-yellow');
-            elementPause.classList.remove('btn-yellow');
-            loop();
+    function playPress() {
+        elementPlay.classList.add('btn-yellow');
+        elementPause.classList.remove('btn-yellow');
+        loop();
 
-            function loop() {
-                subtitlesService.checkSubtitle(video.currentTime * 1000);
-                if (subtitlesService.isSubtitleShow) {
-                    if (subtitlesService.isDraw === false) {
-                        player.pause();
-                        subtitlesService.play(overlayVideo);
-                    } else {
-                        subtitlesService.draw();
-                        player.postprocess.call(subtitlesService, overlayVideo);
-                    }
+        function loop() {
+            subtitleProcessor.checkSubtitle(video.currentTime * 1000);
+            if (subtitleProcessor.isSubtitleShow) {
+                if (subtitleProcessor.isDraw === false) {
+                    player.pause();
+                    subtitleProcessor.play(overlayVideo);
                 } else {
-                    player.play(overlayVideo);
-                    audio.play();
-                    overlayVideo.play();   
+                    subtitleProcessor.draw();
+                    player.postprocess.call(subtitleProcessor, overlayVideo);
                 }
-                rifID = requestAnimationFrame(loop);
-            };
-        }
+            } else {
+                player.play(overlayVideo);
+                audio.play();
+                overlayVideo.play();
+            }
+            rifID = requestAnimationFrame(loop);
+        };
+    }
 
-        function pausePress() {
-            elementPlay.classList.remove('btn-yellow');
-            elementPause.classList.add('btn-yellow');
-            cancelAnimationFrame(rifID);
-            player.pause();
-            overlayVideo.pause();
-            audio.pause();
-            subtitlesService.pause();
-        }
+    function pausePress() {
+        elementPlay.classList.remove('btn-yellow');
+        elementPause.classList.add('btn-yellow');
+        cancelAnimationFrame(rifID);
+        player.pause();
+        overlayVideo.pause();
+        audio.pause();
+        subtitleProcessor.pause();
+    }
 
-        function restartPress() {
-            player.restart();
-            subtitlesService.restart();
-            audio.restart();
-        }
-    });
-
-
-    //function draw() {
-    //    var canvas = document.getElementById("img-1");
-    //    var ctx = canvas.getContext("2d");
-
-    //    ctx.globalCompositeOperation = "multiply";
-
-    //    ctx.fillStyle = "blue";
-    //    ctx.fillRect(10, 1, 100, 100);
-
-    //    ctx.fillStyle = "red";
-    //    ctx.fillRect(50, 50, 100, 100);
-    //};
-
-} 
+    function restartPress() {
+        player.restart();
+        subtitleProcessor.restart();
+        audio.restart();
+    }
+};
--- SHRI.MovieSilencer/client-src/js/subtitleProcessor.js
+++ SHRI.MovieSilencer/client-src/js/subtitleProcessor.js
@@ -4,7 +4,7 @@ const SECOND = 1000,
     MINUTE = 60 * SECOND,
     HOUR = 60 * MINUTE;
 
-class SubtitlesService {
+class SubtitleProcessor {
     constructor(subtitles, player) {
         this.isPause = false;
         this.subtitles = subtitles;
@@ -12,7 +12,7 @@ class SubtitlesService {
         this.canvasContext = player.canvasContext;
         this.player = player;
         this.isDraw = false;
-
+        this.loadSubtitles('attachment/subs.srt');
     }
     checkSubtitle(time) {
         if (this.isSubtitleShow) return;
@@ -64,5 +64,42 @@ class SubtitlesService {
         this.currentIndex = 0;
         this.currentSub = this.subtitles[this.currentIndex];
     }
+    
+    loadSubtitles(stringLoad) {
+        return fetch(stringLoad)
+            .then((responce) => {
+                return responce.text();
+            })
+            .then((data) => {
+                let srt = data.replace(/\r\n|\r|\n/g, '\n');
+                return srt 
+                    .split('\n\n')
+                    .map((subItem) => this.formattedToSubtitle(subItem));
+            });
+    }
+
+    formattedToSubtitle(data) {
+        const subparts = data.split('\n');
+        let time = subparts[1].split(' --> ').map((t) => this.timeParser(t));
+        return new Subtitle({
+            id: subparts[0],
+            startTime: time[0],
+            endTime: time[1],
+            message: subparts.slice(2).join('\n')
+        });
+    }
+    
+    timeParser(timeString) {
+        const chunks = timeString.split(':');
+        const secondChunks = chunks[2].split(',');
+        const hours = parseInt(chunks[0], 10);
+        const minutes = parseInt(chunks[1], 10);
+        const seconds = parseInt(secondChunks[0], 10);
+        const milliSeconds = parseInt(secondChunks[1], 10);
+        return HOUR * hours +
+            MINUTE * minutes +
+            SECOND * seconds +
+            milliSeconds;
+    }
 }
-export {SubtitlesService}
+export {SubtitleProcessor}
